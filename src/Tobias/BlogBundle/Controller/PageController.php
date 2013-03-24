50683b322672058503b5441d2ed9cd28b0dfd87e
Tobias Wooldridge <tobias@wooldridge.id.au>
2013-03-24 13:38:02 +1030

    Modified page controller to use an explicit request object

--- src/Tobias/BlogBundle/Controller/PageController.php
+++ src/Tobias/BlogBundle/Controller/PageController.php
@@ -13,30 +13,27 @@ use Tobias\BlogBundle\Entity\Page;
 /**
  * Page controller.
  *
- * @Route("/page")
- * @Cache(smaxage="1200")
  */
 class PageController extends Controller
 {
     /**
      * Finds and displays a Page entity.
      *
-     * @Route("/{slug}", name="page_show")
-     * @Template()
+     * @Route("/page/{slug}", name="page_show")
      */
     public function showAction($slug)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('TobiasBlogBundle:Page')->findOneBySlug($slug);
+        $page = $em->getRepository('TobiasBlogBundle:Page')->findOneBySlug($slug);
 
-        if (!$entity) {
+        if (!$page)
             throw $this->createNotFoundException('Unable to find Page entity.');
-        }
 
-        return array(
-            'entity'      => $entity,
-        );
+        $response = $this->render('TobiasBlogBundle:Page:show.html.twig', array('page' => $page));
+        $response->setSharedMaxAge(300);
+
+        return $response;
     }
 
 }
